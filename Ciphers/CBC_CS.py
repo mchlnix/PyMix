e6c848d2fc82d18bb91c26e5e0646026cddac98b
Michael Nix <mchl.nix@googlemail.com>
2018-09-17 15:54:48 +0200

    updates doc string for cbc_cs

--- Ciphers/CBC_CS.py
+++ Ciphers/CBC_CS.py
@@ -2,18 +2,43 @@
 algorithm is used in the same way, but instead of inflating the size of the
 mix fragment when encrypting and deflating the size when decrypting, the size
 is kept constant.
-This works by encrypting as normal and assuming the final size, inflated by
-encryption, is the one wanted by the user. That means the given data is
-of size: wanted_size - encrypt_iterations * iv_size. Since the amount of
-iterations is dependent on the number of keys given by the user at time of
-instantiation and the IV size is known or explicitely given by the user, it is
-trivial for them to calculate the necessary size their data has to have in
-order to have a desired size after encryption.
+This works by assuming, that the user, knowing how many random blocks will be
+added by the encryption iteration, will put as many random blocks at the end of
+the plain text, that is encrypted. Since the amount of iterations is dependent
+on the number of keys given by the user at time of instantiation and the IV
+size is known or explicitely given by the user, it is trivial for them to
+calculate the necessary size of the data to be appended.
+The encryption is then adding a random block as normal, but also removing a
+block from the end of the plain text, thereby keeping the size constant.
 At the decryption step the first most block is still discarded, but is replaced
 by a block of random bytes appended to the decrypted packet. It is left to the
 user to either communicate to the ultimate receiver of the decrypted data the
 number of random bytes to discard or to encode that information or the length
-of the actual data inside the sent data itself."""
+of the actual data inside the sent data itself.
+
+Example for 3 iterations and IV size of 16:
+
+    # initial plain data
+    plain
+
+    # after prepare_data
+    plain + r(48)
+
+    # encryption
+    enc(r(16) + plain) + r(32)
+    enc(r(16) + enc(r(16) + plain)) + r(16)
+    enc(r(16) + enc(r(16) + enc(r(16) + plain)))
+
+    # decryption
+    dec(enc(r(16) + enc(r(16) + enc(r(16) + plain)))) =
+    enc(r(16) + enc(r(16) + plain)) + r(16)
+    enc(r(16) + plain) + r(32)
+    plain + r(48)
+
+    # plain data after finalize_data
+    plain
+
+"""
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 
