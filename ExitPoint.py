8a3312a98e7041e27830a9d6f659870997753bb2
Michael Nix <mchl.nix@googlemail.com>
2018-09-18 01:15:05 +0200

    makes ExitPoint into a class

--- ExitPoint.py
+++ ExitPoint.py
@@ -23,99 +23,153 @@ MIN_PORT = 50000
 MAX_PORT = 60000
 
 
-def random_socket():
-    """Returns a socket bound to a random port, that is not in use already."""
+class ExitPoint:
+    def __init__(self, own_addr):
+        self.mix_addr = None
 
-    while True:
-        port = randint(MIN_PORT, MAX_PORT)
+        # a special container, which accepts mix fragments and reassembles them to
+        # MixMessage objects. completed MixMessages can be queried and removed,
+        # their payload is the original unencrypted payload
+        self.store = MixMessageStore()
 
-        if port in ports:
-            # Port already in use by us
-            continue
+        # look up table to map sockets to destinations
+        self.sock_table = TwoWayTable("socket", "channel_id")
 
-        try:
-            new_sock = socket(AF_INET, UDP)
-            new_sock.bind(("127.0.0.1", port))
-            new_sock.setblocking(False)
+        # remember how much padding a plain text has to have (see Cipher/CBC_CS.py)
+        # channel id -> number of padding bytes
+        self.padding_dict = {}
 
-            return new_sock
-        except OSError:
-            # Port already in use by another application, try a new one
-            pass
+        # list of ports, that are listening for responses
+        self.ports = []
 
+        # returns the sockets with data in them, without blocking
+        self.sock_sel = DefaultSelector()
 
-def handle_mix_fragment(packet):
-    """The mix fragment gets added to the fragment store. If the channel id is
-       not already known a socket will be created for the destination of the
-       mix fragment and added to the socket table.
-       If the fragment completes the mix message, all completed mix messages
-       will be sent out over their sockets."""
+        # the socket the mix sends fragments to
+        self.sock_to_mix = socket(AF_INET, UDP)
+        self.sock_to_mix.bind(own_addr)
+        self.sock_to_mix.setblocking(False)
 
-    chan_id = get_chan_id(packet)
-    fragment = get_payload(packet)
+        self.sock_sel.register(self.sock_to_mix, EVENT_READ)
 
-    # the plain text might still have random bytes at the end
-    padding_dict[chan_id] = len(fragment) - FRAG_SIZE
-    print("Plaintext had", padding_dict[chan_id], "bytes padding.")
-    fragment = fragment[0:FRAG_SIZE]
+    def random_socket(self):
+        """Returns a socket bound to a random port, that is not in use already."""
 
-    # add fragment to the mix message store
-    parsed_frag = store.parse_fragment(fragment)
+        while True:
+            port = randint(MIN_PORT, MAX_PORT)
 
-    # is this the first fragment from this channel?
-    if chan_id not in sock_table.channel_ids:
-        # a new udp channel was opened, save mapping and create socket
-        sock_table.socket[chan_id] = random_socket()
+            if port in self.ports:
+                # Port already in use by us
+                continue
 
-        # make sure we only listen to the (eventual) destination of the mix
-        # message
-        sock_table.socket[chan_id].connect(parsed_frag.dest)
+            try:
+                new_sock = socket(AF_INET, UDP)
+                new_sock.bind(("127.0.0.1", port))
+                new_sock.setblocking(False)
 
-        # register socket to the socket selector
-        sock_sel.register(sock_table.socket[chan_id], EVENT_READ)
-
-    # send out the payload of completed MixMessages
-    for mix_message in store.completed():
-        # get the socket associated with the channel
-        sock = sock_table.socket[chan_id]
-
-        # make sure the destination is the same
-        if mix_message.dest != sock.getpeername():
-            raise Exception("The socket is connected to a different " +
-                            "destination, than the mix message is targeting.")
-
-        print(chan_id, "->", mix_message.dest)
-
-        # send complete mix message to the destination
-        sock.send(mix_message.payload)
-
-    # remove sent messages
-    store.remove_completed()
+                return new_sock
+            except OSError:
+                # Port already in use by another application, try a new one
+                pass
 
 
-def handle_response(sock):
-    """Turns the response into a MixMessage and sends its fragments to the mix.
-    """
-    # get response data to set socket.getaddr()
-    response = sock.recv(UDP_MTU)
+    def handle_mix_fragment(self, packet):
+        """The mix fragment gets added to the fragment store. If the channel id is
+           not already known a socket will be created for the destination of the
+           mix fragment and added to the socket table.
+           If the fragment completes the mix message, all completed mix messages
+           will be sent out over their sockets."""
 
-    # get channel id associated with the socket the data came in from
-    chan_id = sock_table.channel_id[sock]
+        chan_id = get_chan_id(packet)
+        fragment = get_payload(packet)
 
-    print(chan_id, "<-", sock.getpeername())
+        # the plain text might still have random bytes at the end
+        self.padding_dict[chan_id] = len(fragment) - FRAG_SIZE
+        if len(fragment) < FRAG_SIZE:
+            print(fragment)
+        print("Plaintext had", self.padding_dict[chan_id], "bytes padding.")
+        fragment = fragment[0:FRAG_SIZE]
 
-    if mix_addr is None:
-        raise Exception("The mix address is not yet known, but there is " +
-                        "already a response for it. Invalid State.")
+        # add fragment to the mix message store
+        parsed_frag = self.store.parse_fragment(fragment)
 
-    # we don't know the actual ip:port of the recipient. the EntryPoint will
-    # get the response through the channel id and will know where to send it
-    mix_frags = make_fragments(response, ("0.0.0.0", 0))
+        # is this the first fragment from this channel?
+        if chan_id not in self.sock_table.channel_ids:
+            # a new udp channel was opened, save mapping and create socket
+            self.sock_table.socket[chan_id] = self.random_socket()
 
-    # append channel id to the fragments
-    for frag in mix_frags:
-        sock_to_mix.send(i2b(chan_id, CHAN_ID_SIZE) +
-                         padded(frag, FRAG_SIZE + padding_dict[chan_id]))
+            # make sure we only listen to the (eventual) destination of the mix
+            # message
+            self.sock_table.socket[chan_id].connect(parsed_frag.dest)
+
+            # register socket to the socket selector
+            self.sock_sel.register(self.sock_table.socket[chan_id], EVENT_READ)
+
+        # send out the payload of completed MixMessages
+        for mix_message in self.store.completed():
+            # get the socket associated with the channel
+            sock = self.sock_table.socket[chan_id]
+
+            # make sure the destination is the same
+            if mix_message.dest != sock.getpeername():
+                raise Exception("The socket is connected to a different " +
+                                "destination, than the mix message is targeting.")
+
+            print(chan_id, "->", mix_message.dest)
+
+            # send complete mix message to the destination
+            sock.send(mix_message.payload)
+
+        # remove sent messages
+        self.store.remove_completed()
+
+
+    def handle_response(self, sock):
+        """Turns the response into a MixMessage and sends its fragments to the mix.
+        """
+        # get response data to set socket.getaddr()
+        response = sock.recv(UDP_MTU)
+
+        # get channel id associated with the socket the data came in from
+        chan_id = self.sock_table.channel_id[sock]
+
+        print(chan_id, "<-", sock.getpeername())
+
+        if self.mix_addr is None:
+            raise Exception("The mix address is not yet known, but there is " +
+                            "already a response for it. Invalid State.")
+
+        # we don't know the actual ip:port of the recipient. the EntryPoint will
+        # get the response through the channel id and will know where to send it
+        mix_frags = make_fragments(response, ("0.0.0.0", 0))
+
+        # append channel id to the fragments
+        for frag in mix_frags:
+            self.sock_to_mix.send(i2b(chan_id, CHAN_ID_SIZE) +
+                                  padded(frag, FRAG_SIZE + self.padding_dict[chan_id]))
+
+    def run(self):
+        while True:
+            events = self.sock_sel.select()
+
+            for key, _ in events:
+                sock = key.fileobj
+
+                # we assume the first message we receive will be from the mix
+                if self.mix_addr is None:
+                    packet, self.mix_addr = sock.recvfrom(UDP_MTU)
+                    sock.connect(self.mix_addr)
+                    self.handle_mix_fragment(packet)
+                    continue
+
+                if sock == self.sock_to_mix:
+                    # collect mix fragments
+                    packet = sock.recv(UDP_MTU)
+                    self.handle_mix_fragment(packet)
+                else:
+                    # send responses to the mix chain
+                    self.handle_response(sock)
+
 
 # pylint: disable=C0103
 if __name__ == "__main__":
@@ -126,61 +180,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    print("Use Ctrl+C to quit.")
-
     own_addr = parse_ip_port(getattr(args, "ip:port"))
     print("Listening on {}:{}".format(*own_addr))
+    print("Use Ctrl+C to quit.")
 
-    mix_addr = None
-
-    # a special container, which accepts mix fragments and reassembles them to
-    # MixMessage objects. completed MixMessages can be queried and removed,
-    # their payload is the original unencrypted payload
-    store = MixMessageStore()
-
-    # look up table to map sockets to destinations
-    sock_table = TwoWayTable("socket", "channel_id")
-
-    # remember how much padding a plain text has to have (see Cipher/CBC_CS.py)
-    # channel id -> number of padding bytes
-    padding_dict = {}
-
-    # list of ports, that are listening for responses
-    ports = []
-
-    # returns the sockets with data in them, without blocking
-    sock_sel = DefaultSelector()
-
-    # the socket the mix sends fragments to
-    sock_to_mix = socket(AF_INET, UDP)
-    sock_to_mix.bind(own_addr)
-    sock_to_mix.setblocking(False)
-
-    sock_sel.register(sock_to_mix, EVENT_READ)
-
-    try:
-        while True:
-            events = sock_sel.select()
-
-            for key, _ in events:
-                sock = key.fileobj
-
-                # we assume the first message we receive will be from the mix
-                if mix_addr is None:
-                    packet, mix_addr = sock.recvfrom(UDP_MTU)
-                    sock.connect(mix_addr)
-                    handle_mix_fragment(packet)
-                    continue
-
-                if sock == sock_to_mix:
-                    # collect mix fragments
-                    packet = sock.recv(UDP_MTU)
-                    handle_mix_fragment(packet)
-                else:
-                    # send responses to the mix chain
-                    handle_response(sock)
-
-    except KeyboardInterrupt as kbi:
-        print("Received Ctrl+C, quitting.")
-        for sock in sock_table.sockets:
-            sock.close()
+    exit_point = ExitPoint(own_addr)
+    exit_point.run()
