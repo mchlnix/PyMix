18c234704672c5b5d55f9d6f7c68fb3b77350d18
Michael Nix <mchl.nix@googlemail.com>
2018-10-28 16:52:22 +0100

    makes Send_Random_Packets take a parameter for the number of packets

--- tests/Send_Random_Packets.py
+++ tests/Send_Random_Packets.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-
-from random import randrange
+from random import randrange, random
 from socket import socket, AF_INET, SOCK_DGRAM as UDP
 from time import time, sleep
+from sys import argv
 
 from util import i2b, ip2b
+
+sleep(random())
 
 MIN_MSG_SIZE = 10
 MAX_MSG_SIZE = 1000 - 6  # ip and port
 
-ip, port = "127.0.0.1", 60001
+ip, port = "127.0.0.1", 45000
 entry_addr = ("127.0.0.1", 20000)
 
-to_send = 1 * 1000 * 1000  # bytes
+if len(argv) > 1:
+    to_send = int(argv[1])
+else:
+    to_send = -1
 
 sock = socket(AF_INET, UDP)
 
@@ -20,20 +25,18 @@ start = time()
 
 packets = 1
 
-while to_send > 0:
+while to_send != 0:
     msg_size = randrange(MIN_MSG_SIZE, MAX_MSG_SIZE)
     pack_id = i2b(packets, 4)
 
     try:
         sock.sendto(ip2b(ip) + i2b(port, 2) + pack_id + bytes(msg_size - 4),
                     entry_addr)
-        print(packets, msg_size)
     except OSError:
         continue
 
-    packets += 1
-    to_send -= msg_size
+    to_send -= 1
 
-    sleep(0.01)
+    sleep(1/20)
 
 print("Sent", packets, "packets in", time() - start, "seconds.")
