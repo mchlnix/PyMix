6a28a58c47d1564d24a8fcb51926f56704f0d29c
Michael Nix <mchl.nix@googlemail.com>
2018-10-28 16:50:30 +0100

    adds 10 random senders to the delay/jitter test

--- tests/Delay-Jitter-Test.sh
+++ tests/Delay-Jitter-Test.sh
@@ -4,18 +4,29 @@ PYTHONPATH="."
 
 export PYTHONPATH
 
-mkdir -p tests/data tests/tmp
-
-tests/Recv_Log.py 127.0.0.1:60001 > tests/tmp/recv-log &
+tests/Recv_and_answer.py 127.0.0.1:45000 >/dev/null &
 
 RECV_PID="$!"
 
 sleep 1
 
+for i in {1..10}; do
+    tests/Send_Random_Packets.py &
+    RECV_PID="$RECV_PID $!"
+done
+
+mkdir -p tests/data tests/tmp
+
+tests/Recv_Log.py 127.0.0.1:60001 > tests/tmp/recv-log &
+
+RECV_PID="$RECV_PID $!"
+
+sleep 1
+
 tests/Replay_Log.py tests/data/discord_call_out > tests/tmp/send-log
 
-sleep 5
+sleep 3
 
-kill "$RECV_PID"
+kill $RECV_PID >/dev/null
 
 tests/measure_delay.py
