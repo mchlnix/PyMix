b517b54399b9ce3c636bc0412dd53665243fa634
Michael Nix <mchl.nix@googlemail.com>
2018-12-15 23:41:15 +0100

    more info in setup py

--- setup.py
+++ setup.py
@@ -5,7 +5,29 @@ setup(
     version="0.9",
     description="MixChain implementation in Python",
     classifiers=[
-        'Programming Language :: Python :: 3',
+        'Development Status :: 3 - Alpha',
+        'Environment :: No Input/Output (Daemon)',
+        'Framework :: Flake8',
+        'Framework :: Pytest',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        #Operating System :: MacOS :: MacOS X
+        #Operating System :: Microsoft :: Windows :: Windows 10
+        #Operating System :: Microsoft :: Windows :: Windows 7
+        #Operating System :: Microsoft :: Windows :: Windows 8
+        #Operating System :: Microsoft :: Windows :: Windows 8.1
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Topic :: Communications',
+        'Topic :: Communications :: Telephony',
+        'Topic :: Internet',
+        'Topic :: Internet :: Proxy Servers',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
         ],
     install_requires=[
         'petlib',
